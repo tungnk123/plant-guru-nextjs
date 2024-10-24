a76bd05a60da55787379700c473c6115f0db0acd
tungnk123 <doanthanhtungnk123@gmail.com>
2024-10-24 20:56:06 +0700

    Add shadcn/ui dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
@@ -6,6 +7,7 @@ const page = () => {
       <section className='py-24'>
         <div className='container'>
           <h1 className='text-3xl font-bold'>Nextjs stater</h1>
+          <Button>Click me</Button>
         </div>
       </section>
     </div>
