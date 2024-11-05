cb3123c9f8e971c72e5ac2059eb8713f5ed7bf2d
tungnk123 <doanthanhtungnk123@gmail.com>
2024-11-05 21:46:11 +0700

    Add status bar below hero section

--- app/home/HeroSection.jsx
+++ app/home/HeroSection.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { Input } from '@/components/ui/input'
-import Image from "next/image";
-import { Search } from 'lucide-react';
+import Image from 'next/image'
+import { Search } from 'lucide-react'
 
 export default function HeroSection() {
   const stats = { plants: 10, posts: 10, users: 10 }
@@ -22,32 +22,49 @@ export default function HeroSection() {
       <Head>
         <title>Plant GURU</title>
       </Head>
-        
-      <header className="heroSection header container mx-auto flex flex-col items-center text-center space-y-4 py-6 mt-10 rounded-3xl shadow-lg">
-        <div className="flex flex-row items-center space-x-2">
-          <Image
-            src="/images/ic_logo.svg"
-            alt="Logo"
-            width={48}  
-            height={48}
-          />
-          <h1 className="inter-medium text-5xl">Plant GURU</h1>
+
+      <header className='heroSection header container mx-auto mt-10 flex flex-col items-center space-y-4 rounded-3xl py-6 text-center shadow-lg'>
+        <div className='flex flex-row items-center space-x-2'>
+          <Image src='/images/ic_logo.svg' alt='Logo' width={48} height={48} />
+          <h1 className='inter-medium text-5xl'>Plant GURU</h1>
         </div>
-        <p className="inter-medium text-2xl ">
-          Your Ultimate Plant Companion
-        </p>
+        <p className='inter-medium text-2xl'>Your Ultimate Plant Companion</p>
         <Input
-          type="text"
-          placeholder="Search for a plant or ask a question..."
-          className="w-full p-2 border border-gray-300 rounded-md bg-white"
+          type='text'
+          placeholder='Search for a plant or ask a question...'
+          className='w-full rounded-md border border-gray-300 bg-white p-2'
         />
-        
       </header>
 
-      <section className="stats flex justify-center space-x-8 mt-6 text-lg text-gray-700">
-        <div>{stats.plants} plants</div>
-        <div>{stats.posts} posts</div>
-        <div>{stats.users} users</div>
+      <section className='stats mt-6 flex justify-center space-x-8 text-lg text-gray-700'>
+        <div className='flex flex-row items-center justify-center'>
+          <Image
+            src='/images/ic_plant_status.svg'
+            alt='Logo'
+            width={32}
+            height={32}
+          />
+          <p className='inter-bold mx-1 mt-1'>{stats.plants}</p> plants
+        </div>
+        <div className='flex flex-row items-center'>
+          <Image
+            src='/images/ic_post_status.svg'
+            alt='Logo'
+            width={32}
+            height={32}
+          />
+           <p className='inter-bold mx-1 mt-1'>{stats.posts}</p> posts
+        </div>
+        <div className='flex flex-row items-center'>
+          <Image
+            src='/images/ic_user_status.svg'
+            alt='Logo'
+            width={32}
+            height={32}
+          />
+          <p className='inter-bold mx-1 mt-1'>{stats.users}</p> users
+
+        </div>
       </section>
     </div>
   )
