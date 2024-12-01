98d2813c26cf90b633bd15f0fdc9c4bf251152d5
tungnk123 <doanthanhtungnk123@gmail.com>
2024-12-01 10:09:05 +0700

    Add all shacn/ui components

--- app/create-post/CreatePost.tsx
+++ app/create-post/CreatePost.tsx
@@ -3,16 +3,10 @@ import Link from 'next/link'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import { Button } from '@/components/ui/button'
 import { PlusIcon } from 'lucide-react'
 import { createPost, PostData } from '@/app/api/postService'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 const CreatePost = () => {
   const [backgroundColor, setBackgroundColor] = useState('#FFFF00')
